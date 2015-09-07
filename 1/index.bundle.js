6cbe00d20d5550bc854cfd8bf435304d21a7fe26
luobotang <tangkangxing@sina.com>
2015-09-07 19:18:48 +0800

    init

--- 1/index.bundle.js
+++ 1/index.bundle.js
@@ -9,7 +9,20 @@ var defaults = {
 	container: '.slide-container',
 	slide: '.slide',
 	slideTime: 1000,
-	active: 0
+	active: 0,
+	scrollNav: true,
+	keyNav: true,
+	navBar: true,
+
+	classSlideIn: 'slide-in',
+	classSlideOut: 'slide-out',
+	classSlideUp: 'slide-up',
+	classSlideDown: 'slide-down',
+	classSlideActive: 'slide-active',
+
+	classSlideNavBar: 'slide-nav-bar',
+	classSlideNav: 'slide-nav',
+	classSlideNavActive: 'slide-nav-active'
 }
 
 function Slider(options) {
@@ -18,63 +31,93 @@ function Slider(options) {
 	var slides = this.slides = this.$el.children(opts.slide)
 
 	var active = this.active = opts.active
-	slides.slice(0, active).addClass('slide-up')
-	slides.eq(active).addClass('slide-active')
-	slides.slice(active + 1).addClass('slide-down')
-	this.enableScroll()
-	this.enableNavBar()
+	slides.slice(0, active).addClass(opts.classSlideUp)
+	slides.eq(active).addClass(opts.classSlideActive)
+	slides.slice(active + 1).addClass(opts.classSlideDown)
+
+	if (opts.scrollNav) this.enableScrollNav()
+	if (opts.keyNav) this.enableKeyNav()
+	if (opts.navBar) this.enableNavBar()
 }
 
-Slider.prototype.enableScroll = function () {
+Slider.prototype.enableScrollNav = function () {
 	var self = this
-	var slideTime = this.opts.slideTime
-
 	this.$el.mousewheel(function (e) {
-		if (self.sliding) return
-
 		var isUp = e.deltaY > 0
-		var prev = self.active
-		var next = isUp ? prev - 1 : prev + 1
-		if (next < 0) return
-		if (next >= self.slides.length) return
-
-		var currentSlide = self.slides.eq(prev)
-		var nextSlide = self.slides.eq(next)
-
-		self.sliding = true
-		self.active = next
-
-		currentSlide
-			.addClass('slide-out')
-			.addClass(isUp ? 'slide-down' : 'slide-up')
-			.removeClass('slide-active')
-		nextSlide
-			.addClass('slide-in')
-			.removeClass(isUp ? 'slide-up' : 'slide-down')
-			.addClass('slide-active')
-
-		setTimeout(function () {
-			self.sliding = false
-			currentSlide.removeClass('slide-out')
-			nextSlide.removeClass('slide-in')
-
-			self.navs.eq(prev).removeClass('slide-nav-active')
-			self.navs.eq(next).addClass('slide-nav-active')
-		}, slideTime)
+		self.nav(isUp)
 	})
 }
 
+var KEY_UP = 38
+var KEY_DOWN = 40
+
+Slider.prototype.enableKeyNav = function () {
+	var self = this
+	$(document).keydown(function (e) {
+		if (e.which === KEY_UP) {
+			self.nav(true)
+		} else if (e.which === KEY_DOWN) {
+			self.nav(false)
+		}
+	})
+}
+
+Slider.prototype.nav = function (isUp) {
+	var opts = this.opts
+	var self = this
+	var slideTime = this.opts.slideTime
+
+	if (self.sliding) return
+
+	var prev = self.active
+	var next = isUp ? prev - 1 : prev + 1
+	if (next < 0) return
+	if (next >= self.slides.length) return
+
+	var currentSlide = self.slides.eq(prev)
+	var nextSlide = self.slides.eq(next)
+
+	self.sliding = true
+	self.active = next
+
+	var classSlideIn = opts.classSlideIn
+	var classSlideOut = opts.classSlideOut
+	var classSlideUp = opts.classSlideUp
+	var classSlideDown = opts.classSlideDown
+	var classSlideActive = opts.classSlideActive
+	var classSlideNavActive = opts.classSlideNavActive
+
+	currentSlide
+		.addClass(classSlideOut)
+		.addClass(isUp ? classSlideDown : classSlideUp)
+		.removeClass(classSlideActive)
+	nextSlide
+		.addClass(classSlideIn)
+		.removeClass(isUp ? classSlideUp : classSlideDown)
+		.addClass(classSlideActive)
+
+	setTimeout(function () {
+		self.sliding = false
+		currentSlide.removeClass(classSlideOut)
+		nextSlide.removeClass(classSlideIn)
+
+		self.navs.eq(prev).removeClass(classSlideNavActive)
+		self.navs.eq(next).addClass(classSlideNavActive)
+	}, slideTime)
+}
+
 Slider.prototype.enableNavBar = function () {
+	var opts = this.opts
 	var total = this.slides.length
-	var $bar = $('<ul class="slide-nav-bar"></ul>')
+	var $bar = $('<ul class="' + opts.classSlideNavBar + '"></ul>')
 	for (var i = 0; i < total; i++) {
-		$bar.append('<li class="slide-nav"></li>')
+		$bar.append('<li class="' + opts.classSlideNav + '"></li>')
 	}
 
 	$bar.appendTo(this.$el)
 
 	var navs = this.navs = $bar.find('li')
-	navs.eq(this.active).addClass('slide-nav-active')
+	navs.eq(this.active).addClass(opts.classSlideNavActive)
 }
 
 module.exports = Slider
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # slide-animation
-JavaScript + CSS animation
+
+幻灯片切换效果。
+
+## 说明
+
+一张幻灯片页面经历的状态包括：
+
+- slide-in
+- slide-active
+- slide-out
+
+## 使用方法
+
+```javascript
+var $ = require('jquery')
+var Slider = require('luobo-slider')
+new Slider()
+```
+
+## 依赖
+
+依赖 jQuery
+依赖 jquery-mousewheel
